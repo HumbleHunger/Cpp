80b294e7476fa407f2722fc15d9af51322db7677
HumbleHunger <2495970924@qq.com>
2020-09-24 23:45:51 +0800

    a

--- class.cpp
+++ class.cpp
@@ -44,37 +44,11 @@ test f()
 }
 int main()
 {
-    /*{
-        sub *p=new sub[5];
-        for(int i=0;i<5;i++){
-            p[i].test1(i);
-        }
-        //p[0].insert(999);
-        p[0].set(1000);
-        //p--;
-        delete []p;
-    }*/
-    //sub p(1890);
-    //p.f(1);
-    //test *zz=(test *)&p;
-    
-    //p.f(2,22);
-    //zz->print();
-    //group h;
-    //test q(455);
-    //const test& y=f();
-    //test* z=&q;
     test q,w;
     q.insert(119);
     w.print();
     w.insert(110);
     q.print();
-    //y.print();
-    //y.print();
-    //y.b=20;
-    //f()=q;
-    //f().print();
-    //cout << "f().b is "<< f().b << " over"<< endl;
 
 
 
