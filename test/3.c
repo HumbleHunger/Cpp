8c3d3c401bd13c275078073a2b6599a38b4b7b2a
HumbleHunger <2495970924@qq.com>
2020-11-10 20:48:38 +0800

    s

--- test/3.c
+++ test/3.c
@@ -5,11 +5,3 @@
 	> Created Time: 2020年10月30日 星期五 12时34分32秒
  ************************************************************************/
 
-#include<stdio.h>
-int main()
-{
-    char ch[17]="Xiyou_Linux_Group";
-    char *str="Xiyou_Linux_Group";
-    printf("%s\n%s\n",ch,str);
-    return 0;
-}
