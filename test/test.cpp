da1b3289995ac100174727a1436cd9f2da2d48f8
HumbleHunger <2495970924@qq.com>
2020-12-22 19:06:12 +0800

    S

--- test/test.cpp
+++ test/test.cpp
@@ -8,28 +8,6 @@
 int main()
 {
     /*
-    String s1("abc");
-    cout << "jlk"+s1 << endl;
-    String s2(s1);
-    cout << s2 << endl;
-    String s3=s1;
-    cout << s3<< endl;
-    String s4="abc";
-    String s5(3,'a');
-    String s6(s1+"def");
-    String s7;
-    ifstream in("1.c");
-    getline(in,s7);
-    for(auto a : s1){
-        cout << a <<endl;
-    }
-    cout << s2 << endl;
-    cout << s2.size() << endl;
-    cout << s3.empty() << endl;
-    cout << s4 << endl;
-    cout << s5 << endl;
-    cout << s6 << endl;
-    cout << s7 << endl;
 */
     string s={'a','b','v','s','e','d'};
     String s1("aaaaaaaaaaaaa");
